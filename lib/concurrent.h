b1d846282f77b9f3af4b96995e312fb10841c3cf
rccuam <racc@correo.azc.uam.mx>
2020-10-24 18:08:33 -0500

    simplificación y speedup a concurrent.h
    Ahora el concurrent_output_iterator directamente es el que guarda el atomic. Aprovechando que hay un único productor, se aceleran las operaciones atómicas.

--- lib/concurrent.h
+++ lib/concurrent.h
@@ -9,9 +9,65 @@
 
 namespace lib {
    template<typename IT>
+   class concurrent_output_iterator {
+      std::atomic<IT> iter;
+   public:
+      using value_type = std::iterator_traits<IT>::value_type;
+      using pointer = value_type*;
+      using reference = value_type&;
+      using difference_type = std::ptrdiff_t;
+      using iterator_category = std::iterator_traits<IT>::iterator_category;
+
+      concurrent_output_iterator(IT p)
+      : iter(p) {
+      }
+
+      value_type& operator*( ) {
+         return *iter.load(std::memory_order_relaxed);
+      }
+
+      const value_type& operator*( ) const {
+         return *iter.load(std::memory_order_relaxed);
+      }
+
+      concurrent_output_iterator& operator++( ) {
+         iter.store(iter.load(std::memory_order_relaxed) + 1, std::memory_order_release);
+         return *this;
+      }
+
+      auto operator++(int) {
+         struct postincrement_proxy {
+            concurrent_output_iterator& actual;
+         public:
+            ~postincrement_proxy( ) {
+               ++actual;
+            }
+
+            value_type& operator*( ) {
+               return *actual;
+            }
+         };
+         return postincrement_proxy(*this);
+      }
+
+      concurrent_output_iterator& operator+=(std::ptrdiff_t i) {
+         iter.store(iter.load(std::memory_order_relaxed) + i, std::memory_order_release);
+         return *this;
+      }
+
+      const std::atomic<IT>& atomic_iterator( ) const {
+         return iter;
+      }
+
+      operator IT( ) const {
+         return iter.load(std::memory_order_relaxed);
+      }
+   };
+
+   template<typename IT>
    class concurrent_inspect_iterator {
       IT iter;
-      std::reference_wrapper<const std::atomic<IT>> fin;
+      std::reference_wrapper<const concurrent_output_iterator<IT>> fin;
    public:
       using value_type = const std::iterator_traits<IT>::value_type;
       using pointer = const value_type*;
@@ -19,12 +75,12 @@ namespace lib {
       using difference_type = std::ptrdiff_t;
       using iterator_category = std::forward_iterator_tag;
 
-      concurrent_inspect_iterator(IT i, const std::atomic<IT>& f)
+      concurrent_inspect_iterator(IT i, const concurrent_output_iterator<IT>& f)
       : iter(i), fin(f) {
       }
 
       value_type& operator*( ) const {
-         while (iter >= fin.get( )) {
+         while (iter >= fin.get( ).atomic_iterator( ).load(std::memory_order_acquire)) {
             continue;
          }
          return *iter;
@@ -50,58 +106,10 @@ namespace lib {
       }
    };
 
-   template<typename IT>
-   class concurrent_output_iterator {
-      std::reference_wrapper<std::atomic<IT>> iter;
-   public:
-      using value_type = std::iterator_traits<IT>::value_type;
-      using pointer = value_type*;
-      using reference = value_type&;
-      using difference_type = std::ptrdiff_t;
-      using iterator_category = std::forward_iterator_tag;
-
-      concurrent_output_iterator(std::atomic<IT>& f)
-      : iter(f) {
-      }
-
-      value_type& operator*( ) {
-         return *iter.get( );
-      }
-
-      concurrent_output_iterator& operator++( ) {
-         ++iter.get( );
-         return *this;
-      }
-
-      auto operator++(int) {
-         struct postincrement_proxy {
-            concurrent_output_iterator& actual;
-         public:
-            ~postincrement_proxy( ) {
-               ++actual;
-            }
-
-            value_type& operator*( ) {
-               return *actual;
-            }
-         };
-         return postincrement_proxy(*this);
-      }
-
-      concurrent_output_iterator& operator+=(std::ptrdiff_t i) {
-         iter.get( ) += i;
-         return *this;
-      }
-
-      operator pointer( ) {
-         return iter.get( );
-      }
-   };
-
    template<typename T>
    class concurrent_buffer {
       std::unique_ptr<T[]> mem;
-      std::atomic<T*> iter_fin;
+      concurrent_output_iterator<T*> iter_fin;
       std::size_t cap;
    public:
       using value_type = T;
@@ -120,30 +128,6 @@ namespace lib {
          return cap;
       }
 
-      value_type& operator[](std::size_t i) {
-         return begin( )[i];
-      }
-
-      const value_type& operator[](std::size_t i) const {
-         return begin( )[i];
-      }
-
-      value_type& front( ) {
-         return *begin( );
-      }
-
-      const value_type& front( ) const {
-         return *begin( );
-      }
-
-      value_type& back( ) {
-         return *std::prev(end( ));
-      }
-
-      const value_type& back( ) const {
-         return *std::prev(end( ));
-      }
-
       iterator begin( ) {
          return mem.get( );
       }
@@ -160,12 +144,12 @@ namespace lib {
          return mem.get( ) + size( );
       }
 
-      auto output_iterator( ) {
-         return concurrent_output_iterator(iter_fin);
+      auto& output_iterator( ) {
+         return iter_fin;
       }
 
       auto inspect_iterator( ) const {
-         return concurrent_inspect_iterator(begin( ), iter_fin);
+         return concurrent_inspect_iterator<T*>(begin( ), iter_fin);
       }
    };
 }
