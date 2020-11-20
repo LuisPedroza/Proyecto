f03092db261d3309b6414f0eb95f8f6fd1553986
LuisPedroza <36962624+LuisPedroza@users.noreply.github.com>
2020-11-20 17:02:53 -0600

    Nuevo benchmark

--- benchmark.bat
+++ benchmark.bat
@@ -3,19 +3,9 @@ FOR /L %%A IN (1,1,5) DO (
     main.exe test\test_5.txt secuencial
 )
 
-echo "Concurrente .5 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_5.txt
-)
-
 echo "Secuencial 1 MB"
 FOR /L %%A IN (1,1,5) DO (      
     main.exe test\test_1.txt secuencial
-)
-
-echo "Concurrente 1 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_1.txt
 )
 
 echo "Secuencial 2 MB"
@@ -23,19 +13,9 @@ FOR /L %%A IN (1,1,5) DO (
     main.exe test\test_2.txt secuencial
 )
 
-echo "Concurrente 2 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_2.txt
-)
-
 echo "Secuencial 4 MB"
 FOR /L %%A IN (1,1,5) DO (      
     main.exe test\test_4.txt secuencial
-)
-
-echo "Concurrente 4 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_4.txt
 )
 
 echo "Secuencial 8 MB"
@@ -43,19 +23,9 @@ FOR /L %%A IN (1,1,5) DO (
     main.exe test\test_8.txt secuencial
 )
 
-echo "Concurrente 8 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_8.txt
-)
-
 echo "Secuencial 16 MB"
 FOR /L %%A IN (1,1,5) DO (      
     main.exe test\test_16.txt secuencial
-)
-
-echo "Concurrente 16 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_16.txt
 )
 
 echo "Secuencial 32 MB"
@@ -63,9 +33,5 @@ FOR /L %%A IN (1,1,5) DO (
     main.exe test\test_32.txt secuencial
 )
 
-echo "Concurrente 32 MB"
-FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_32.txt
-)
 
 pause
