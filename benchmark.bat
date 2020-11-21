a78b97b655ff41630857432764291d3b437e1f11
LuisPedroza <36962624+LuisPedroza@users.noreply.github.com>
2020-11-20 18:40:44 -0600

    Correccion al benchmark

--- benchmark.bat
+++ benchmark.bat
@@ -1,36 +1,36 @@
-echo "Secuencial .5 MB"
+echo "debug -t .5 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_5.txt secuencial
+    main.exe test\test_5.txt debug -t
 )
 
-echo "Secuencial 1 MB"
+echo "debug -t 1 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_1.txt secuencial
+    main.exe test\test_1.txt debug -t
 )
 
-echo "Secuencial 2 MB"
+echo "debug -t 2 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_2.txt secuencial
+    main.exe test\test_2.txt debug -t
 )
 
-echo "Secuencial 4 MB"
+echo "debug -t 4 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_4.txt secuencial
+    main.exe test\test_4.txt debug -t
 )
 
-echo "Secuencial 8 MB"
+echo "debug -t 8 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_8.txt secuencial
+    main.exe test\test_8.txt debug -t
 )
 
-echo "Secuencial 16 MB"
+echo "debug -t 16 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_16.txt secuencial
+    main.exe test\test_16.txt debug -t
 )
 
-echo "Secuencial 32 MB"
+echo "debug -t 32 MB"
 FOR /L %%A IN (1,1,5) DO (      
-    main.exe test\test_32.txt secuencial
+    main.exe test\test_32.txt debug -t
 )
 
 
